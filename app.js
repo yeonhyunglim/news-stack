1126c42543cca6708b1268604c72b4aaeb4380d4
yeonhyunglim <43203651+yeonhyunglim@users.noreply.github.com>
2018-12-05 16:10:53 +0900

    주석 수정
    코드를 수정하였습니다.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-/* test: abcdefghijikkk */
+/* test: abcdefghijikkk * Github/
 
 /* Session */
 const session = require('express-session');
